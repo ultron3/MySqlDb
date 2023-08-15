01dd80975ea5793da7b631594c5b14acb91aaeef
ultron3 <alex.gnavi@gmail.com>
2023-08-15 10:31:11 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using MySql.Data.MySqlClient;
+
+namespace MySQLExample
+{
+
+    //comando per installare la libreria 
+    //dotnet add package MySql.Data
+    public class Program
+    {
+       
+
+        public static void Main(string[] args)
+        {
+            string connectionString = "server=localhost;user=root;password=telemedicina2123;database=db_pharm;";
+
+            // Creazione del database
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS Ixla";
+                using (MySqlCommand cmd = new MySqlCommand(createDatabaseQuery, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            // Creazione della tabella
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string createTableQuery = "CREATE TABLE IF NOT EXISTS UserIxla (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT)";
+                using (MySqlCommand cmd = new MySqlCommand(createTableQuery, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("Database and table created successfully.");
+            
+            
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Inserimento dei dati nella tabella
+                string insertDataQuery = "INSERT INTO UserIxla (name, age) VALUES (@name, @age)";
+                using (MySqlCommand cmd = new MySqlCommand(insertDataQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@name", "John Doe");
+                    cmd.Parameters.AddWithValue("@age", 30);
+                    cmd.Parameters.AddWithValue("@name", "John Snow");
+                    cmd.Parameters.AddWithValue("@age", 306);
+                    cmd.ExecuteNonQuery();
+                    
+                }
+            }
+
+            Console.WriteLine("record add successfully");
+        }
+    }
+}
